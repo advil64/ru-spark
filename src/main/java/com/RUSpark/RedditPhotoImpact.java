ec57e40bf19064d3476b0d2d97e85aa1fc953673
advil64 <advil64@gmail.com>
2021-11-29 20:39:24 -0500

    started reddit photos

--- src/main/java/com/RUSpark/RedditPhotoImpact.java
+++ src/main/java/com/RUSpark/RedditPhotoImpact.java
@@ -1,20 +1,31 @@
 package com.RUSpark;
 
-/* any necessary Java packages here */
+//tutorial: https://www.hellocodeclub.com/apache-spark-java-tutorial-simplest-guide-to-get-started/
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+
 
 public class RedditPhotoImpact {
 
 	public static void main(String[] args) throws Exception {
 
-    if (args.length < 1) {
-      System.err.println("Usage: RedditPhotoImpact <file>");
-      System.exit(1);
-    }
-		
-		String InputPath = args[0];
-		
-		/* Implement Here */ 
-		
-	}
+        if (args.length < 1) {
+          System.err.println("Usage: RedditPhotoImpact <file>");
+          System.exit(1);
+        }
+
+        // dataset path
+        String InputPath = args[0];
+
+        // logger and other configurations
+        String logFile = "/common/home/ac1771/Desktop/ru-spark/log.md";
+        SparkConf conf = new SparkConf().setAppName("redditPhotoImpact").setMaster("local[3]");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+
+        // load the dataset into a spark rdd
+        JavaRDD<String> photosData = sparkContext.textFile(InputPath);
+
+        }
 
 }
